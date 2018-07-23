f329625b09c4e3c4e778c1453ce9d8934edd8632
braiser <braiserpangemanan@gmail.com>
2018-07-23 13:24:02 +0800

    edit form edit surat keputusan

--- application/views/surat_keputusan/surat_keputusan_edit.php
+++ application/views/surat_keputusan/surat_keputusan_edit.php
@@ -13,7 +13,7 @@
 <div class="row">
     
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <h1 class="page-header"><i class="fas fa-plus"></i> Tambah Surat Masuk</h1>
+        <h1 class="page-header"><i class="fas fa-plus"></i> Edit Surat Surat Keputusan</h1>
     </div>
 </div>
 
